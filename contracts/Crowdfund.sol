6327b1d923dfd9c67123a50f0d86acdada808171
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-12 17:42:58 -0400

    crowdfunding contract update

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -2,3 +2,24 @@
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
+
+contract CrowdfundConstructor{
+
+}
+
+
+
+contract CrowdfundingProject{
+    uint public projectId;
+    string public projectName;
+    uint public startTime;
+
+    constructor(uint id, string memory _projectName){
+        projectId = id;
+        projectName = _projectName;
+        startTime = block.timestamp;
+    }
+
+    
+
+}
--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,25 +3,44 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token{
-    string public name;
-    string public symbol;
-    uint public decimals = 18;
-    uint public totalSupply;
 
-    address public owner;
+contract Token is ERC20, ERC20Burnable, Pausable, Ownable{
 
-    //mapping of balances
-    mapping(address=>uint) public balancesOf;
+    uint256 public _totalSupply;
 
-    constructor(string memory _name, string memory _symbol, uint _totalSupply){
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _totalSupply;
-        owner = payable(msg.sender);
-        balancesOf[msg.sender] = _totalSupply;
+    mapping(address=>uint) public _balances;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
+        _balances[address(this)] = _totalSupply;
+        
     }
 
+
+    
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
 }
